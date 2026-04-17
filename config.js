27e0eff6ee18decf5e8983e7397c8dcf9435cd78
agitkasep <agitkasep80@gmail.com>
2026-04-17 10:54:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// CONFIGURASI OTOMATIS PAK AGIT
+const SB_URL = "https://dhtezbjltciwqytgkbma.supabase.co"; 
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRodGV6YmpsdGNpd3F5dGdrYm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMjA3NTksImV4cCI6MjA5MTc5Njc1OX0.q8xVCso4aSBmR6OWF9YBdAYmRaVOrlE34p7YB7TxACw";
+
+// Inisialisasi (Sudah siap pakai)
+const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
