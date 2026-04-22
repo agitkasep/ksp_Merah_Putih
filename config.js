0025e49eb54994ec80a4df0ff17538d809360fbb
agitkasep <agitkasep80@gmail.com>
2026-04-23 01:28:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
-const supabaseUrl = 'https://dhtezbjltciwqytgkbma.supabase.co';
-const supabaseKey = 'sb_publishable_fe28rSS0YMz9Ld2OLJGi8w_jDV7uswE'; 
-const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+// ==========================================
+// FILE: config.js
+// Pusat Konfigurasi Database KOPDESKEL
+// ==========================================
+
+const SB_URL = "https://dhtezbjltciwqytgkbma.supabase.co"; 
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRodGV6YmpsdGNpd3F5dGdrYm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMjA3NTksImV4cCI6MjA5MTc5Njc1OX0.q8xVCso4aSBmR6OWF9YBdAYmRaVOrlE34p7YB7TxACw";
+
+// PERUBAHAN PENTING: Kita beri nama variabelnya 'supabaseClient'
+const supabaseClient = window.supabase.createClient(SB_URL, SB_KEY);
+
+console.log("Koneksi Supabase Siap Digunakan!");
