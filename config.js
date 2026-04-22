f8ccf8553a4cd73ca773dce685b7e0d77d5ed374
agitkasep <agitkasep80@gmail.com>
2026-04-23 01:12:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,3 @@
-// ==========================================
-// FILE: config.js
-// Pusat Konfigurasi Database KOPDESKEL
-// ==========================================
-
-// Gunakan URL dan Key JWT Anon asli dari kodingan lama Bapak
-const SB_URL = "https://dhtezbjltciwqytgkbma.supabase.co"; 
-const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRodGV6YmpsdGNpd3F5dGdrYm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMjA3NTksImV4cCI6MjA5MTc5Njc1OX0.q8xVCso4aSBmR6OWF9YBdAYmRaVOrlE34p7YB7TxACw";
-
-// Inisialisasi Supabase
-const supabase = window.supabase.createClient(SB_URL, SB_KEY);
-
-// Jembatan untuk halaman lama Bapak agar tidak error
-const supabaseClient = supabase; 
-
-console.log("Database KOPDESKEL Terhubung Sukses!");
+const supabaseUrl = 'https://dhtezbjltciwqytgkbma.supabase.co';
+const supabaseKey = 'sb_publishable_fe28rSS0YMz9Ld2OLJGi8w_jDV7uswE'; 
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
